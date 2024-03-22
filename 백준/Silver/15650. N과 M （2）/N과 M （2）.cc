c2c9c7ca85582aca486eb36ceb3ff41f483fd86c
Miller <69894461+KimDaeSeong8721@users.noreply.github.com>
2024-03-22 13:11:25 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -7,9 +7,7 @@
 using namespace std;
 
 int arr[20];
-int isused[20];
 int n,m ;
-
 
 void func2(int k, int prev) {
    if(k == m){ // m개를 모두 택했으면
@@ -20,11 +18,8 @@ void func2(int k, int prev) {
   }
 
     for(int i = prev + 1; i <= n; i++){ 
-    if(isused[i]) continue;
       arr[k] = i;  
-      isused[i] = 1; // i를 사용되었다고 표시
       func2(k+1, i); // 다음 수를 정하러 한 단계 더 들어감
-      isused[i] = 0; // k번째 수를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용되지않았다고 명시함.
     
   }
 }
