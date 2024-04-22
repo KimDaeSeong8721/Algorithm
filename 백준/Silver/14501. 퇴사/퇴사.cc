c0ece016e67c38fc8769c364138fe3526e7b1bbb
Miller <69894461+KimDaeSeong8721@users.noreply.github.com>
2024-04-22 21:50:38 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,43 +1,32 @@
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
+
+int n ; 
+int d[16];
 int t[16];
 int p[16];
-int tempTot;
-int maxTot;
-int n;
-void dfs(int cnt) {
-  if (cnt == n) {
-    maxTot = max(tempTot, maxTot);
-    // tempTot = 0 ;
-    return;
-  }
-  if (t[cnt] + cnt < n) {
-    tempTot += p[cnt];
-    dfs(cnt + t[cnt] + 1);
-    tempTot -= p[cnt];
-  } 
-  dfs(cnt + 1);
-
-}
 
 int main() {
   ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    int a;
-    int b;
-    cin >> a >> b;
-    t[i] = a - 1;
-    p[i] = b;
-  }
+    cin.tie(0); 
+    cin >> n ;
+    
+    for(int i = 0 ; i < n ; i++){
+        int a; int b ;
+        cin >> a >> b;
+        t[i] = a ;
+        p[i] = b ;
+    }
 
-  for (int i = 0; i < n; i++) {
-    dfs(i);
-  }
-
-  cout << maxTot;
+    for(int i = n-1 ; i >= 0 ; i--) {
+        if(i + t[i] <= n) {
+            d[i] = max(d[i+t[i]]+p[i], d[i+1]);
+        }
+        else d[i] = d[i+1];
+      
+    }
+    cout << *max_element(d,d+n);
 }
