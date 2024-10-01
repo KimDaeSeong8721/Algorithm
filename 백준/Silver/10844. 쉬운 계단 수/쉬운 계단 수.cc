5ba154085f399813a84d540fd8ffe493accd5171
Miller <69894461+KimDaeSeong8721@users.noreply.github.com>
2024-10-01 12:34:11 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -2,27 +2,25 @@
 #include <algorithm>
 using namespace std;
 
+typedef long long ll ; 
+ll dp[101][10] ;
 
+ll sumV ; 
 int n ;
-
-long long dp[101][10];
 int main() {
   ios::sync_with_stdio(0);
     cin.tie(0); 
     cin >> n ;
-    
-    dp[1][0] = 0 ;
-    for(int j = 1 ; j < 10 ; j++) dp[1][j] = 1;
-
-
-    for(int i = 2 ; i <= 100 ; i++)
-        for(int j = 0 ; j < 10 ; j++){
-            if(j == 0) dp[i][0] = dp[i-1][1] % 1'000'000'000 ;
-            else if(j == 9) dp[i][9] = dp[i-1][8] % 1'000'000'000;
-            else dp[i][j] = dp[i-1][j-1] % 1'000'000'000 + dp[i-1][j+1] % 1'000'000'000;
+    for(int j = 1 ; j <= 9 ; j++) dp[1][j] = 1 ;
+    for(int i = 2; i <= n ; i++) {
+	    for(int j = 0 ; j <= 9 ; j++) {
+	    if(j == 0) dp[i][j] = dp[i-1][j+1];
+	    else if(j == 9) dp[i][j] = dp[i-1][j-1];
+	    else dp[i][j] =  dp[i-1][j-1] + dp[i-1][j+1]; 
+        dp[i][j] %= 1'000'000'000;
+            }   
         }
-        
-    long long sum = 0 ;
-    for(int j= 0 ; j < 10 ; j++) sum += dp[n][j] % 1'000'000'000; 
-    cout << sum % 1'000'000'000 ; 
+
+    for(int j = 0 ; j <= 9 ; j++) sumV += dp[n][j] ;
+    cout << sumV % 1'000'000'000 ;
 }
