8542373523593da509fce12e1bd6067c2dd7f224
Miller <69894461+KimDaeSeong8721@users.noreply.github.com>
2025-02-16 14:43:03 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+
+int dp[1001];
+
+int main() {
+  ios::sync_with_stdio(0);
+    cin.tie(0); 
+    int n ; 
+    cin >> n ;
+    
+    dp[1] = -1;
+    dp[3] = -1;
+    dp[2] = 1;
+    for(int i = 4 ; i <= n ; i++) {
+        if(dp[i-3] != 0) {
+            dp[i] = -dp[i-3];
+        } else {
+            dp[i] = -dp[i-1];
+        }
+    }
+    if(dp[n] == -1) {
+        cout << "SK";
+    } else {
+        cout << "CY";
+    }
+}
