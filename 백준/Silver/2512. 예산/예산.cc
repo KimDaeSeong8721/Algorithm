b51d26dc1987f0a7836d076b66fc5dadaa8bc6c6
Miller <69894461+KimDaeSeong8721@users.noreply.github.com>
2025-04-11 00:20:00 +0900

    [Silver II] Title: 예산, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+
+
+
+int n ;
+int m; 
+vector<int> v ; 
+bool check(int k) {
+    long long  sum = 0 ;
+    for(auto e : v) {
+        if(k - e > 0){
+            sum += e ;
+        } else {
+            sum += k ; 
+        }
+    }
+    return sum <= m ; 
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+    cin.tie(0); 
+
+    cin >> n ; 
+    while (n--)
+    {
+        int tmp ; 
+        cin >> tmp ;
+        v.push_back(tmp);
+    }
+    cin >> m ; 
+
+    int lo = 1 ;
+    int hi = *max_element(v.begin(), v.end());
+    
+    while (lo + 1 < hi)
+    {
+        int mid = (lo + hi) / 2  ; 
+        if(check(mid)) {
+            lo = mid ;
+        } else {
+            hi = mid -1;
+        }
+    }
+    if (check(hi)) {
+        cout << hi;
+    } else {
+        cout << lo;
+    }
+}
